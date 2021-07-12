b259550941a755d5c753b9e7c9130eb61dedfcc3
Arnold Olanda <olanda188@gmail.com>
2021-07-12 18:30:11 -0500

    V 0.1 Se agregaron las clases hashc y register para trabajar con tablas de hash, se implemento la clase ContenedorLocales que almacena en una tabla hash los diferentes locales de vacunacion

---
+++ ContenedorLocales.java
@@ -0,0 +1,28 @@
+
+
+public class ContenedorLocales {
+	private HashC<Local> contenedor;
+	
+	public ContenedorLocales() {
+		this.contenedor=new HashC<Local>(10);
+	}
+	
+	public void registrarNuevoLocal(int key,Local l) {
+		this.contenedor.insert(key, l);
+	}
+	
+	public Local getLocal(int key) {
+		Local l=this.contenedor.searchRegister(key);
+		if(l!=null) {
+			return l;
+		}else {
+			return null;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return this.contenedor.toString();
+	}
+	
+}
