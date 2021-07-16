7ee0533540f129a3a11e5e4dadf079c9d0ec4c89
christian Chili <christian.chili9@gmail.com>
2021-07-16 18:57:42 -0500

    Se completo la interfazEnviarVacuna y el login

--- src/Login.java
+++ src/Login.java
@@ -3,21 +3,27 @@ import java.awt.*;
 import java.awt.Color;
 import java.awt.event.*;
 public class Login extends JFrame implements ActionListener, ItemListener {
-	private Local reflocal;
+	private ContenedorLocales reflistaLocales;
+	private HashC reflistaUsu;
+	private Mapa refmapa;
 	private JLabel label1, label2, label3, LBI1, LBI2, LBI3;
 	private JTextField t1;
 	private JPasswordField t2;
 	private JButton BT1, BT2;
 	private JComboBox CB1;
 	private String seleccion;
+	private Local localSelc;
 	private ImageIcon image1, image2, image3;
 	
 	String a,b;
 	
 	
-	public Login(Local reflocal) {
-		this.reflocal=reflocal;
-		this.seleccion="Seleccione";
+	public Login(ContenedorLocales listaLocales,HashC listaUsu,Mapa refmapa) {
+		this.refmapa= refmapa;
+		this.seleccion= "";
+		this.reflistaUsu= listaUsu;
+		this.reflistaLocales = listaLocales;
+	
 		
 		this.getContentPane().setBackground(Color.decode("#d2fdbc"));
 		setLayout(null);
@@ -26,14 +32,14 @@ public class Login extends JFrame implements ActionListener, ItemListener {
 		label1 = new JLabel("Usuario: ");
 		label1.setBounds(60,75,100,30);
 		add(label1);
-		t1 = new JTextField();
+		t1 = new JTextField("");
 		t1.setBounds(150,80,150,20);
 		add(t1);
 		
 		label2 = new JLabel("Contraseña: ");
 		label2.setBounds(60,100,100,30);
 		add(label2);
-		t2 = new JPasswordField();
+		t2 = new JPasswordField("");
 		t2.setBounds(150,105,150,20);
 		add(t2);
 		
@@ -42,10 +48,12 @@ public class Login extends JFrame implements ActionListener, ItemListener {
 		add(label3);
 		CB1 = new JComboBox();
 		CB1.setBounds(150,130,150,20);
-		CB1.addItem("Item 1");
-		CB1.addItem("Item 2");
-		CB1.addItem("Item 3");
-		CB1.addItem("Item 4");
+		
+		Node <Local> aux=this.reflistaLocales.listaLocales().getFirst();
+		CB1.addItem("");
+		for(int i =  0 ; aux!=null ; aux = aux.getNext(),i++)
+			CB1.addItem(aux.getData().getNombre());
+		CB1.addItemListener(this);
 		add(CB1);
 		
 		 	
@@ -93,43 +101,60 @@ public class Login extends JFrame implements ActionListener, ItemListener {
 		LBI3 = new JLabel(fin2);
 		LBI3.setBounds(115,15,100,60);
 		add(LBI3);
-		
-		
-		
+					
 	}
 	
 	
 	public void actionPerformed(ActionEvent e) {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		if(e.getSource() == BT1) {
-			String nombre="", contra="";
-			Usuario Us = new Usuario();
-			nombre = t1.getText().trim();
-			contra = t2.getText().trim();
-			String resul = Us.validarUsuario(nombre, contra);
-			JOptionPane.showMessageDialog(BT1,resul);
-			if(resul.equals("Acceso denegado"))
-			{
+			
+			if(this.verificarUsuario(t1.getText()) && this.verificarContra(t2.getText()) &&this.verificarSelecion()) {
 				
-				JOptionPane.showMessageDialog(null,"vuelve a logearte");
-				t1.setText(null);
-				t2.setText(null);
-			}
-			else {
-				Principal principal1 = new Principal(reflocal);
-				principal1.setTitle("Menu Principal");
-				principal1.setBounds(0,0,400,500);
-				principal1.setVisible(true);
-				principal1.setLocationRelativeTo(null);
-				principal1.setDefaultCloseOperation(EXIT_ON_CLOSE);
-				this.setVisible(false);
+				int nombre =0; 
+				String contra;
 				
+				nombre = Integer.parseInt(t1.getText());
+				contra = t2.getText();
+				
+				PersonalSalud aux= (PersonalSalud) this.reflistaUsu.searchRegister(nombre);
+				String resul="Acceso denegado";
+				if(aux!=null) {
+					 resul = aux.validarUsuario(nombre, contra);
+				}		
+					
+				
+				JOptionPane.showMessageDialog(BT1,resul);
+				if(resul.equals("Acceso denegado"))
+				{
+					JOptionPane.showMessageDialog(null,"vuelve a logearte");
+					t1.setText(null);
+					t2.setText(null);
+				}
+				else {
+					this.setVisible(false);
+					
+					if(aux.getTipy()=="normal") {
+						Interfaz_menu_personal_salud salud = new Interfaz_menu_personal_salud(this.localSelc,this,aux.getNombre());
+						salud.setTitle("Menu Principal");
+						salud.setLocationRelativeTo(null);
+						salud.setVisible(true);
+						
+					}else {
+						Interfaz_sava admin= new Interfaz_sava(reflistaLocales,this.localSelc,this,aux.getNombre(),this.refmapa);
+						admin.setTitle("Menu Principal");
+						admin.setVisible(true);
+						admin.setLocationRelativeTo(null);
+					}
+					t1.setText(null);
+					t2.setText(null);
+				}
 			}
+			
 		}
 		
 		if(e.getSource()==BT2) {
-			t1.setText(null);
-			t2.setText(null);
+			System.exit(0);
 		}
 	}
 	public void mostrar_interfaz() {
@@ -140,14 +165,52 @@ public class Login extends JFrame implements ActionListener, ItemListener {
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
 	}
+	
+	
 	public void itemStateChanged(ItemEvent e) {
 		if(e.getSource()==CB1) {
 			this.seleccion= (String) CB1.getSelectedItem();
+			Node <Local> aux=this.reflistaLocales.listaLocales().getFirst();
+			for(int i =  0 ; aux!=null ; aux = aux.getNext(),i++) {
+				if(aux.getData().getNombre()==this.seleccion) {
+					this.localSelc= aux.getData();
+				}
+			}
+			
 		}
-		
-		CB1.addItem("Seleccione");
-		CB1.setVisible(true);
 	}
-
 	
+	private boolean verificarSelecion() {
+		if(this.seleccion=="") {
+			JOptionPane.showMessageDialog(null,"Error: No eligio un local");
+			return false;
+		}
+		else
+			return true;
+	}
+	private boolean verificarUsuario(String usuario) {
+		if(usuario.length()==0) {
+			JOptionPane.showMessageDialog(null,"Error: No ingreso su usuario");
+			return false;
+		}
+		for(int i =0;i<usuario.length();i++) {
+			if((usuario.charAt(i)<'0' || usuario.charAt(i)>'9')) {
+				JOptionPane.showMessageDialog(null,"Error: Se ingreso una letra");
+				return false;
+			}
+		}
+		if(usuario.length()!=4) {
+			JOptionPane.showMessageDialog(null,"Error: usuario no igual a 4 digitos");
+			return false;
+		}
+		return true;
+	}
+	private boolean verificarContra(String contra) {
+		if(contra.length()==0) {
+			JOptionPane.showMessageDialog(null,"Error: No ingreso su contraseña");
+			return false;
+		}
+		else
+			return true;
+	}
 }
